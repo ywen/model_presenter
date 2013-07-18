e65b4209a7a43228fde0685f24f30190c739d02c
Yi Wen <ywen.github@gmail.com>
2013-07-17 21:53:16 -0500

    Add an example

--- lib/model_presenter/has_many.rb
+++ lib/model_presenter/has_many.rb
@@ -1,11 +1,18 @@
 module ModelPresenter
   module HasMany
-    # The DSL adds an instance method `relation`
+    # The DSL adds an instance method `relation` to represnet a `has_many` relationship
+    #
+    # ```ruby
+    # class User
+    #   include ModelPresenter::Base
+    #   has_many :groups, presenter_class: Presenters::Group
+    # end
+    # ```
+    #
+    # The macro will generates a ```groups``` methods, which will return an array. Each element of the array is an instance of ```Presenters::Group``` whose ```model``` is one of the group models that the user has.
     #
     # @param [Symbol, #read] relation the name of the relationship
-    # @param [Hash, #read] options Currently it accepts one key `presenter_class` is a class. The class is
-    # supposed to be a presenter class. The `relation` method will return an
-    # array of elements that are instances of the class
+    # @param [Hash, #read] options Currently it accepts one key `presenter_class` is a class. The class is supposed to be a presenter class. The `relation` method will return an array of elements that are instances of the class
     #
     # @return none
     def has_many(relation, options)
