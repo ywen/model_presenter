623bb60bfd8ee9fdda64c2b4524a1f99e7d9f51a
Yi Wen <ywen.github@gmail.com>
2013-07-17 22:04:34 -0500

    Use rdoc format for inline code

--- lib/model_presenter/has_many.rb
+++ lib/model_presenter/has_many.rb
@@ -1,6 +1,6 @@
 module ModelPresenter
   module HasMany
-    # The DSL adds an instance method `relation` to represnet a `has_many` relationship
+    # The DSL adds an instance method +relation+ to represnet a +has_many+ relationship
     #
     #
     #     class User
@@ -8,10 +8,10 @@ module ModelPresenter
     #       has_many :groups, presenter_class: Presenters::Group
     #     end
     #
-    # The macro will generates a ```groups``` methods, which will return an array. Each element of the array is an instance of ```Presenters::Group``` whose ```model``` is one of the group models that the user has.
+    # The macro will generates a +groups+ methods, which will return an array. Each element of the array is an instance of +Presenters::Group+ whose +model+ is one of the group models that the user has.
     #
     # @param [Symbol, #read] relation the name of the relationship
-    # @param [Hash, #read] options Currently it accepts one key `presenter_class` is a class. The class is supposed to be a presenter class. The `relation` method will return an array of elements that are instances of the class
+    # @param [Hash, #read] options Currently it accepts one key +presenter_class+ is a class. The class is supposed to be a presenter class. The +relation+ method will return an array of elements that are instances of the class
     #
     # @return none
     def has_many(relation, options)
