a87ce372ccdf9baa7144ed89fba72420025c4cfa
Yi Wen <ywen.github@gmail.com>
2013-07-17 21:39:57 -0500

    Start to add rdoc to th has_many

--- lib/model_presenter/has_many.rb
+++ lib/model_presenter/has_many.rb
@@ -1,5 +1,13 @@
 module ModelPresenter
   module HasMany
+    # The DSL adds an instance method `relation`
+    #
+    # @param [Symbol, #read] the name of the relationship
+    # @param [Hash, #read] key `presenter_class` is a class. The class is
+    # supposed to be a presenter class. The `relation` method will return an
+    # array of elements that are instances of the class
+    #
+    # @return none
     def has_many(relation, options)
       self.send :define_method, relation do
         instance_variable_name = "@#{relation}"
